6e3252b6024e7659ee8cf5791078b3e4bb5bc03a
jackfioru92 <56474340+jackfioru92@users.noreply.github.com>
2026-02-24 10:51:10 +0100

    ingradito foto centrale

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,33 @@ module.exports = {
           '100%': { transform: 'translateY(0)', opacity: '1' },
         },
       },
+      fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'base': '1rem',
+        'medium': '1.2rem', // Adjusted to ensure it differs from 'base'
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '3.75rem',
+        '7xl': '4.5rem',
+        '8xl': '6rem',
+        '9xl': '8rem',
+      },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 550, // Updated from 500 to 550
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
     },
   },
   plugins: [
